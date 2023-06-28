3ddff0ee26ddaeee35127f2d49444366a46ed0a1
JotaG <115374810+jotaguerrero@users.noreply.github.com>
2023-06-27 23:13:23 -0300

    Added requirements for Mac and Linux users

--- python/README.md
+++ python/README.md
@@ -1,3 +1,23 @@
+## Requirements
+
+In order to use the script you'll need to install some libraries. You'll find them in the `requirements.txt` file that you can install with the command `pip install -r requirements.txt`. In order to run the scripts that uses the tkinter library you need to have it previously installed in your system, you can do so by running these commands:
+
+### Mac OS
+
+- `brew install tkinter`
+
+### For Debian-based Linux:
+
+- `sudo apt install python-tk`
+
+### For Arch-based Linux:
+
+- `sudo pacman -S tk`
+
+### Windows:
+
+- If you installed the `requirements.txt` file, the tk library should be enough to run the scripts
+
 ## [Drop 'N' Save Duplicates](drop-n-save-duplicates.py)
 
 - Run the script and a window will pop up (python3 drop-n-save-duplicates.py)
@@ -21,10 +41,18 @@
 - The script will automatically and merge all .xlsx files into a single DataFrame
 - The user will be prompted to select a file name and directory to save the merged DataFrame
 
-## [Query a CSV file using SQL Syntax](query-csv.py)
+## [Query a CSV file using SQL Syntax](query-csv-gui.py)
 
 - Run the script and a window will pop up (python3 query-csv.py)
 - Select up to two .csv files as DataFrames to query from (named database1 and database2 respectively)
 - Write your query in the text box using SQL syntax
 - Press the "Run Query" button and the output will appear in the terminal
 - The user will be prompted to select a file name and directory to save the output as a .csv file
+
+## [Query a CSV file using SQL Syntax (just in case the 'gui' version doesn't work)](query-csv.py)
+
+- Run the script and a window will pop up (python3 query-csv.py)
+- Select up to two .csv files as DataFrames to query from (named database1 and database2 respectively)
+- Modify the query inside the code before executing
+- The user will be prompted to select a file name and directory to save the output as a .csv file
+- Results will be also printed to the terminal
--- python/query-csv.py
+++ python/query-csv.py
@@ -2,120 +2,43 @@ import pandas as pd
 import duckdb
 import tkinter as tk
 from tkinter import filedialog
-from tkinter import ttk
 
-# Define the window / font theme colors
-bg_color = "#333333"
-fg_color = "#FFFFFF"
-text_bg_color = "#202020"
-text_fg_color = "#FFFFFF"
-
-def select_file(file_label):
-    # Open a dialog box to select the CSV file
-    file_path = filedialog.askopenfilename(title="Select a CSV file", filetypes=(("CSV files", "*.csv"),))
-    if file_path:
-        file_label.config(text=file_path)
-
-def run_query():
-    # Read the CSV files with pandas to set the DataFrame to query from
-    file_path1 = file_label1.cget("text")
-    database1 = pd.read_csv(file_path1) if file_path1 else None
-    # [Optional] Read a second CSV file and join the resulting DataFrames in the query box
-    file_path2 = file_label2.cget("text")
-    database2 = pd.read_csv(file_path2) if file_path2 else None
-
-    # Get the query from the text box
-    query = query_text.get("1.0", tk.END).strip()
-
-    # Execute the query
-    if database1 is not None:
-        df = duckdb.query(query).df()
-    else:
-        df = duckdb.query(query, database1=database1, database2=database2).df()
-
-    # Save the output to a new CSV file
-    save_path = filedialog.asksaveasfilename(title="Save CSV file", defaultextension=".csv", filetypes=(("CSV files", "*.csv"),))
-    if save_path:
-        df.to_csv(save_path, index=False)
-
-    # Print the resulting DataFrame from the query
-    print(df)
-
-# Create the main window
+# Start tkinter
 root = tk.Tk()
-root.title("📊 CSV Query Tool 🛠️")
+root.withdraw()
 
-# Configure padding
-root.configure(padx=20, pady=20)
+# Open a dialog box to select the CSV file
+file_path1 = filedialog.askopenfilename(title="Select a CSV file", filetypes=(("CSV files", "*.csv"),))
+file_path2 = filedialog.askopenfilename(title="Select a CSV file", filetypes=(("CSV files", "*.csv"),))
 
-# Set the window background color
-root.configure(background=bg_color)
+# Read the CSV file with pandas to set the DataFrame
+database1 = pd.read_csv(file_path1)
+database2 = pd.read_csv(file_path2)
 
-# Create a frame for the file selection
-file_frame1 = ttk.Frame(root)
-file_frame1.pack(pady=10)
+# Create a temporary DuckDB connection
+con = duckdb.connect(database=':memory:')
 
-# Set the frame background color
-file_frame1.configure(style="FileFrame.TFrame")
+# Register the DataFrames as tables in DuckDB
+con.register('database1', database1)
+con.register('database2', database2)
 
-# Create file selection objects
-file_label1 = ttk.Label(file_frame1, text="", foreground=fg_color, background=bg_color)
-file_label1.pack(side="left", padx=10)
+# Modify the query below as needed
+query = """
+SELECT *
+FROM database1 a
+LEFT JOIN database2 b ON a.user_id = b.user_id
+LIMIT 10;
+"""
+df = con.execute(query).fetchdf()
 
-select_button1 = ttk.Button(file_frame1, text="Select File (database1)", command=lambda: select_file(file_label1))
-select_button1.pack(side="left")
+# Open a dialog box to save the output
+save_path = filedialog.asksaveasfilename(title="Save CSV file", defaultextension=".csv", filetypes=(("CSV files", "*.csv"),))
 
-clear_button1 = ttk.Button(file_frame1, text="Clear File", command=lambda: file_label1.config(text=""))
-clear_button1.pack(side="left", padx=10)
+# Save the output to a new CSV file
+df.to_csv(save_path, index=False)
 
-# Create a frame for the optional second file selection
-file_frame2 = ttk.Frame(root)
-file_frame2.pack(pady=10)
+# Close the connection
+con.close()
 
-# Set the frame background color
-file_frame2.configure(style="FileFrame.TFrame")
-
-# Create file selection objects
-file_label2 = ttk.Label(file_frame2, text="", foreground=fg_color, background=bg_color)
-file_label2.pack(side="left", padx=10)
-
-select_button2 = ttk.Button(file_frame2, text="Select File (database2)", command=lambda: select_file(file_label2))
-select_button2.pack(side="left")
-
-clear_button2 = ttk.Button(file_frame2, text="Clear File", command=lambda: file_label2.config(text=""))
-clear_button2.pack(side="left", padx=10)
-
-# Create a frame for the query input
-query_frame = ttk.Frame(root)
-query_frame.pack(pady=10)
-
-# Set the frame background color
-query_frame.configure(style="QueryFrame.TFrame")
-
-# Create a label and text box for the query
-query_label = ttk.Label(query_frame, text="Write your query:\n", foreground=fg_color, background=bg_color)
-query_label.pack()
-
-query_text = tk.Text(query_frame, height=15, width=70, bg=text_bg_color, fg=text_fg_color,  insertbackground=fg_color)
-query_text.pack()
-
-# Set the query sample
-query_sample = "SELECT *\nFROM database1\n"
-query_text.insert("1.0", query_sample)
-
-# Create a button to run the query
-run_button = ttk.Button(root, text="Run Query", command=run_query)
-run_button.pack(pady=10)
-
-# Apply styles
-style = ttk.Style()
-
-style.configure("TFrame", background=bg_color)
-style.configure("TLabel", foreground=fg_color, background=bg_color)
-style.configure("TButton", foreground=fg_color, background=bg_color, padx=5, pady=5)
-
-style.configure("FileFrame.TFrame", background=bg_color)
-style.configure("QueryFrame.TFrame", background=bg_color)
-
-# Start the main event loop
-root.mainloop()
+# Print the resulting DataFrame from the query
+print(df)
